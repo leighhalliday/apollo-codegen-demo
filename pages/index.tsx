e428d0d92bb6758ee7edad13edaf450a450beae7
Leigh Halliday <leighhalliday@gmail.com>
2021-01-19 09:40:03 -0500

    Finished

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,25 @@
 import { useQuery, gql } from "@apollo/client";
+import { HomeQuery, HomeQueryVariables } from "src/generated/HomeQuery";
+
+const HOME_QUERY = gql`
+  query HomeQuery($search: String!) {
+    names(search: $search)
+  }
+`;
 
 export default function Home() {
-  return <div>Home</div>;
+  const { data, loading } = useQuery<HomeQuery, HomeQueryVariables>(
+    HOME_QUERY,
+    {
+      variables: { search: "ma" },
+    }
+  );
+
+  if (loading || !data) return <div>loading...</div>;
+
+  return (
+    <div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
---
+++ src/generated/HomeQuery.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: HomeQuery
+// ====================================================
+
+export interface HomeQuery {
+  names: string[];
+}
+
+export interface HomeQueryVariables {
+  search: string;
+}
